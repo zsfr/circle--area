3d2cd6ce5f373f8ad52b72e93defad280bf98d2d
1 <1991847230@qq.com>
2017-04-27 22:45:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
-#include<iostream>
-#include"circle.h"
+#include "head.h"
 using namespace std;
 
 int main()
 {
-	double r,s;//s代表圆的面积，r代表圆的半径。
-	cin >> r;
-	s = circle(r);//函数实现的是输入圆的半径，然后计算圆的面积。
-	cout << s << endl;
+	double r,s;             //定义半径为r,面积为s
+	cin>>r;                 //输入r
+	s=Area(r);              //调用函数area()，求得面积
+	output(s);              //调用函数output()，输出面积
 	return 0;
 }
