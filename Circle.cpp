45d084400cbab18627beeb4ce6962942416e920c
1 <1991847230@qq.com>
2017-04-27 22:56:55 +0800

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
-#include"caculate.h"
+//外部函数实现
+
+#include"head.h"
 using namespace std;
-double caculate(double r)
+
+double Area(double r)
 {
-	double s;
-	s = 3.14*r*r;
-	return s;
+	return (3.14*r*r);//计算圆的面积
+}
+
+void output(double s)
+{
+	cout<<"面积是"<<s;//输出结果
 }
